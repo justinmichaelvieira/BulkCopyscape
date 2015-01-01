ef46c848e7e967663e7d6fce2980f60eadca8894
Justin Angryrancor <angryrancor@dissentingmedia.com>
2015-01-01 13:04:04 -1000

    Create RunCopyscapeLoop.php

---
+++ RunCopyscapeLoop.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Perform Bulk Copyscape</title>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+
+</head>
+<body id="main_body" >
+	
+	<img id="top" src="top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>Perform Bulk Copyscape</a></h1>
+		<form id="form_949981" class="appnitro" enctype="multipart/form-data" method="post" action="">
+					<div class="form_description">
+			<h2>Perform Bulk Copyscape</h2>
+			<p>Select a file below and click "Go" to run the bulk copyscript operation.</p>
+		</div>						
+			<ul >
+			
+					<li id="li_1" >
+		<label class="description" for="element_1">Upload a .zip File </label>
+		<div>
+			<input id="element_1" name="element_1" class="element file" type="file"/> 
+		</div>  
+		</li>
+			
+					<li class="buttons">
+			    <input type="hidden" name="form_id" value="949981" />
+			    
+				<input id="saveForm" class="button_text" type="submit" name="submit" value="Go" />
+		</li>
+			</ul>
+		</form>	
+		<div id="footer">
+			&nbsp;
+		</div>
+	</div>
+	<img id="bottom" src="bottom.png" alt="">
+	</body>
+</html>
