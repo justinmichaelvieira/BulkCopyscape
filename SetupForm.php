f7e49b753fce1329366121487b790ae9950b660f
Justin Angryrancor <angryrancor@dissentingmedia.com>
2015-01-01 10:37:21 -1000

    Update SetupForm.php

--- SetupForm.php
+++ SetupForm.php
@@ -2,13 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>Bulk Copyscape Setup</title>
-<link rel="stylesheet" type="text/css" href="./SetupForm_files/view.css" media="all">
-<script type="text/javascript" src="./SetupForm_files/view.js"></script>
-<script type="text/javascript" src="./SetupForm_files/calendar.js"></script>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+<script type="text/javascript" src="calendar.js"></script>
 <style>img.chromoji { width:1.2em !important; height:1.2em !important; }.chromoji-font, #chromoji-font { font-size:1.2em !important; }</style></head>
 <body id="main_body" class="safari">
 	
-	<img id="top" src="./SetupForm_files/top.png" alt="">
+	<img id="top" src="top.png" alt="">
 	<div id="form_container">
 	
 		<h1><a>Bulk Copyscape Setup Form</a></h1>
@@ -42,6 +42,6 @@
 			&nbsp; &nbsp;
 		</div>
 	</div>
-	<img id="bottom" src="./SetupForm_files/bottom.png" alt="">
+	<img id="bottom" src="bottom.png" alt="">
 	
 <iframe style="display: none !important; position: fixed !important; padding: 0px !important; margin: 0px !important; left: 0px !important; top: 0px !important; width: 100% !important; height: 100% !important; z-index: 2147483647 !important; border: none !important; background-color: transparent !important;"></iframe><span class="_hsShareImage hsShareImage" style="display: none;">&nbsp;</span><span style="margin: 0px auto; border: 2px dotted rgb(255, 165, 0); position: absolute; z-index: 2147483647; visibility: hidden; left: 708px; width: 0px; top: 36px; height: 0px;"></span><span style="z-index: 2147483647; position: absolute; visibility: hidden; left: 693px; width: 50px; top: 21px; height: 20px; font-size: 10px; color: black;"></span></body></html>
