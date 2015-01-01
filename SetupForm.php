b55d0d0dbaace95ba98df10b9f01598ba82fb35e
Justin Angryrancor <angryrancor@dissentingmedia.com>
2015-01-01 09:41:15 -1000

    initial

---
+++ SetupForm.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Bulk Copyscape Setup</title>
+<link rel="stylesheet" type="text/css" href="./SetupForm_files/view.css" media="all">
+<script type="text/javascript" src="./SetupForm_files/view.js"></script>
+<script type="text/javascript" src="./SetupForm_files/calendar.js"></script>
+<style>img.chromoji { width:1.2em !important; height:1.2em !important; }.chromoji-font, #chromoji-font { font-size:1.2em !important; }</style></head>
+<body id="main_body" class="safari">
+	
+	<img id="top" src="./SetupForm_files/top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>Bulk Copyscape Setup Form</a></h1>
+		<form id="form_949981" class="appnitro" method="post" action="#">
+					<div class="form_description">
+			<h2>Bulk Copyscape Setup</h2>
+			<p>please get the details below at <a href="http://copyscape.com" alt="copyscape">http://copyscape.com</a>, and be sure to add some cash to your premium account.</p>
+		</div>						
+			<ul>
+			
+					<li id="li_1">
+		<label class="description" for="element_1">Copyscape Username </label>
+		<div>
+			<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""> 
+		</div><p class="guidelines" id="guide_1"><small>Get this at http://copyscape.com</small></p> 
+		</li>		<li id="li_2">
+		<label class="description" for="element_2">Copyscape API key </label>
+		<div>
+			<input id="element_2" name="element_2" class="element text medium" type="text" maxlength="255" value=""> 
+		</div><p class="guidelines" id="guide_2"><small>Get this at http://copyscape.com</small></p> 
+		</li>
+			
+					<li class="buttons">
+			    <input type="hidden" name="form_id" value="949981">
+			    
+				<input id="saveForm" class="button_text" type="submit" name="submit" value="Save">
+		</li>
+			</ul>
+		</form>	
+		<div id="footer">
+			&nbsp; &nbsp;
+		</div>
+	</div>
+	<img id="bottom" src="./SetupForm_files/bottom.png" alt="">
+	
+<iframe style="display: none !important; position: fixed !important; padding: 0px !important; margin: 0px !important; left: 0px !important; top: 0px !important; width: 100% !important; height: 100% !important; z-index: 2147483647 !important; border: none !important; background-color: transparent !important;"></iframe><span class="_hsShareImage hsShareImage" style="display: none;">&nbsp;</span><span style="margin: 0px auto; border: 2px dotted rgb(255, 165, 0); position: absolute; z-index: 2147483647; visibility: hidden; left: 708px; width: 0px; top: 36px; height: 0px;"></span><span style="z-index: 2147483647; position: absolute; visibility: hidden; left: 693px; width: 50px; top: 21px; height: 20px; font-size: 10px; color: black;"></span></body></html>
