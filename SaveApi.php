c8959f76b268e2433207f2ec38cda5926d2b9ae5
Justin Angryrancor <justin@gmail.com>
2015-01-02 00:39:02 -0500

    works better now

--- SaveApi.php
+++ SaveApi.php
@@ -1,22 +1,44 @@
 <html>
 <body>
-<? 
 
-//Makes user dir for us if it doesn't exist
-function file_force_contents($filename, $data, $flags = 0){
-    if(!is_dir(dirname($filename)))
-        mkdir(dirname($filename).'/', 0644, TRUE);
-    return file_put_contents($filename, $data,$flags);
+<?php
+
+$file1= "c:\\inetpub\\wwwroot\\test1\\uploads\\BulkCSConfig.json";
+$debug = 0;
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', "c:/php.log");
+
+function mk_file($filename) {
+    if(!is_file($filename)) {
+		$handle = fopen($filename,"w"); //create the file
+		$string = "module.exports = {u: " . $_POST['element_1'] . ", k: " . $_POST['element_2'] . "}";
+		fwrite($handle, $string); 
+		
+        fclose($handle);
+		
+        return true;
+    } else return false; //file already exists
+}
+
+
+if($debug)
+{
+	foreach ($_POST as $key => $value)
+		echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
 }
 
 //Write config file
-$wrote = file_force_contents('./uploads/BulkCSConfig.json', 'module.exports = {u: ' + $_POST['element_1'] +', k: ' + $_POST['element_2'] + '}');
+$wrote = mk_file($file1);
 
-//Display error or sucess message
+//Display error or success message
 if($wrote)
-	echo "Saved api settings. Now you can go to " +  + " to use the tool.";
+	echo "Saved api settings. Now you can go to use the tool.  <a href='RunCopyscapeLoop.php'> Click here to continue...</a>";
 else 
-	trigger_error("file_force_contents() : error writing file '$wrote'", E_USER_WARNING); 
+	echo "Error writing file - make sure BulkCSConfig.json does not already exist in the upload dir, and upload dir is writeable."; 
 ?>
+
 </body>
 </html>
