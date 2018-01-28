1849b428df9cd1e324c18ae0b3b438fdf69e85a6
Justin Vieira <justin@rancorsoft.com>
2018-01-28 16:52:02 -0500

    Nav callbacks; File selects; Move to ruamel.yaml from pyyaml

--- BulkCopyScape-pyqt/bulkcopyscape/__main__.py
+++ BulkCopyScape-pyqt/bulkcopyscape/__main__.py
@@ -1,6 +1,7 @@
 """Startup script for the app"""
-import logging, sys, yaml
+import logging, sys
 from PyQt5.QtWidgets import QApplication
+import ruamel.yaml as yaml
 from .utilities.File import loadOrCreateFile
 from .widgets import AboutWidget, ConfigForm, MainWindow, UploadWidget
 from . import __version__
@@ -18,18 +19,21 @@ def main():
     app.setOrganizationDomain("rancorsoft.com")
     app.setApplicationName("BulkCopyScape")
     mainWindow = MainWindow.MainWindow()
-    aboutWidget = AboutWidget.AboutWidget()
+    navToUploadScreen = lambda: mainWindow.pageStack.setCurrentIndex(0)
+    navToAboutScreen = lambda: mainWindow.pageStack.setCurrentIndex(1)
+    navToConfigScreen = lambda: mainWindow.pageStack.setCurrentIndex(2)
+    aboutWidget = AboutWidget.AboutWidget(navToUploadScreen)
     config = yaml.safe_load(loadOrCreateFile("config.yml"))  # Load config
-    configForm = ConfigForm.ConfigForm(config)
-    uploadWidget = UploadWidget.UploadWidget()
+    configForm = ConfigForm.ConfigForm(config, navToUploadScreen)
+    uploadWidget = UploadWidget.UploadWidget(navToAboutScreen, navToConfigScreen)
 
     for widget in [uploadWidget, aboutWidget, configForm]:
         mainWindow.pageStack.addWidget(widget)
 
     if config is None:
-        mainWindow.pageStack.setCurrentIndex(2)
+        navToConfigScreen()
     else:
-        mainWindow.pageStack.setCurrentIndex(0)
+        navToUploadScreen()
 
     mainWindow.showFullScreen()
     sys.exit(app.exec_())  # Without this, the script exits immediately.
--- BulkCopyScape-pyqt/bulkcopyscape/utilities/File.py
+++ BulkCopyScape-pyqt/bulkcopyscape/utilities/File.py
@@ -1,4 +1,5 @@
 """Handles files, file types and file transforms"""
+import ruamel.yaml as yaml
 
 
 def loadOrCreateFile(filename):
@@ -7,3 +8,8 @@ def loadOrCreateFile(filename):
     except FileNotFoundError:
         f = open(filename, 'w')
     return f
+
+
+def saveConfig(apiUser, apiKey):
+    with open("config.yml", 'w') as f:
+        yaml.safe_dump({"apiUser": apiUser, "apiKey": apiKey}, f)
