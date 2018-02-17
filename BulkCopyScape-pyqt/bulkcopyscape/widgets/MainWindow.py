900e7d8e54a0b52d5eb64b801fc715212b0e92f6
Justin Vieira <justin@rancorsoft.com>
2018-02-16 19:34:50 -0500

    Add quit on esc key

--- BulkCopyScape-pyqt/bulkcopyscape/widgets/MainWindow.py
+++ BulkCopyScape-pyqt/bulkcopyscape/widgets/MainWindow.py
@@ -1,4 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QMainWindow, QShortcut, QApplication
+
 from .mainwindow_ui import Ui_MainWindow
 
 
@@ -6,3 +9,5 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(self.__class__, self).__init__(parent)
         self.setupUi(self)
+        self.exitShortcut = QShortcut(QKeySequence(Qt.Key_Escape), self)
+        self.exitShortcut.activated.connect(lambda: QApplication.quit())
